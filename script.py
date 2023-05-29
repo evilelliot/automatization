5d9f078d3ee5b9ae7b9dede74b3e7a7a735f6a26
evielliot <zap01462@zapopan.tecmm.edu.mx>
2023-05-29 04:06:40 -0600

    Ahora si es CLI: actualizado el código en script.py, ahora permite la entrada de datos a través del CLI.

--- script.py
+++ script.py
@@ -1,5 +1,19 @@
+import argparse
 from src.config_reader import ConfigReader
 
-file_path = "./config/config.json"
+# Crear el analizador de argumentos
+parser = argparse.ArgumentParser(description='Script de configuración')
+parser.add_argument('-f', '--file', type=str, help='Ruta al archivo de configuración')
+
+# Obtener los argumentos de la línea de comandos
+args = parser.parse_args()
+
+# Verificar si se proporcionó un archivo de configuración
+if args.file:
+    file_path = args.file
+else:
+    raise ValueError("Debe proporcionar la ruta al archivo de configuración utilizando el argumento -f/--file")
+
+# Crear una instancia de ConfigReader y procesar el archivo de configuración
 config_reader = ConfigReader(file_path)
 config_reader.print_formatted_json()
