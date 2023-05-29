33272f8d1340bbf85ba9165c79e7afaced56786f
evielliot <zap01462@zapopan.tecmm.edu.mx>
2023-05-29 03:22:34 -0600

    Añadiendo un nuevo formato de colores para la impresión de los datos: ahora los items se muestran como item : valor en lugar de aparecer todos iguales.

--- script.py
+++ script.py
@@ -2,5 +2,4 @@ from src.config_reader import ConfigReader
 
 file_path = "./config/config.json"
 config_reader = ConfigReader(file_path)
-formatted_json = config_reader.format_json()
-print(formatted_json)
+config_reader.print_formatted_json()
--- src/config_reader.py
+++ src/config_reader.py
@@ -1,16 +1,39 @@
 import json
+from colorama import Fore, Style
+
 
 class ConfigReader:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def format_json(self):
+    def format_json(self, data, indent_level=0):
+        formatted_output = ""
+        indent = "  " * indent_level
+        for key, value in data.items():
+            formatted_output += f"{indent}{Fore.GREEN}{key}:{Style.RESET_ALL} "
+            if isinstance(value, dict):
+                formatted_output += "\n" + self.format_json(value, indent_level + 1)
+            elif isinstance(value, list):
+                formatted_output += "[\n"
+                for item in value:
+                    if isinstance(item, dict):
+                        formatted_output += self.format_json(item, indent_level + 2)
+                    else:
+                        formatted_output += f"{indent}  {item}\n"
+                formatted_output += f"{indent}]\n"
+            else:
+                formatted_output += f"{value}\n"
+        return formatted_output
+
+    def print_formatted_json(self):
         try:
             with open(self.file_path) as file:
                 data = json.load(file)
-                formatted_json = json.dumps(data, indent=4)
-                return formatted_json
+                formatted_json = self.format_json(data)
+                print(formatted_json)
         except FileNotFoundError:
-            return f"Archivo no encontrado: {self.file_path}"
+            print(f"Archivo no encontrado: {self.file_path}")
         except json.JSONDecodeError:
-            return f"Error al decodificar el archivo JSON: {self.file_path}"
+            print(f"Error al decodificar el archivo JSON: {self.file_path}")
+
+
